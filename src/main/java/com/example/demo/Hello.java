95d5717688f2ce85e0c72ae7c4b06fe6db86ad55
henkeh <heinrich.henke@telefonica.com>
2025-02-10 10:32:14 +0100

    commit-1

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Hello {
+    String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
